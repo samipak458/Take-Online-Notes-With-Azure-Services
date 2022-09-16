040bf049a8db74564afaa705a176be164be6ad51
MUHAMMAD SAMIULLAH <samipak458@gmail.com>
2022-09-16 21:03:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,20 +45,6 @@ const App = () => {
     localStorage.setItem('notes', JSON.stringify(notes));
   }, [notes]);
 
-   /***************************** Saving All Notes In Local Storage  *****************************/
-  useEffect(() => {
-    const savedTheme = JSON.parse(localStorage.getItem('theme'));
-
-    if (savedTheme) {
-      setDarkMode(savedTheme);
-    }
-
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('theme', JSON.stringify(darkMode));
-  }, darkMode);
-
   return (
     <>
       <div className={`${darkMode && 'dark-mode'}`}>
