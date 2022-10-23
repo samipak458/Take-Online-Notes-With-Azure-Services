b19caca6731b656300cb94a382ef8979854b2cfe
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2022-10-24 01:26:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ const App = () => {
 
   return (
     <>
-      <div className={`${darkMode && 'bg-[#1f1f1f] min-h-screen'}`}>
+      <div className={`${darkMode && 'dark-mode'}`}>
         <Header handleDarkMode={setDarkMode} />
 
         <div className='flex flex-col gap-5 my-8 w-[90vw] md:w-[70vw] lg:w-[50vw] mx-auto'>
